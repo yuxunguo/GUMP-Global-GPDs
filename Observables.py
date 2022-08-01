9311490f426ee9e445b48cd53be11a7f493188fb
Yuxun Guo <youuungx@gmail.com>
2022-08-01 12:18:00 -0400

    Upd

--- Observables.py
+++ Observables.py
@@ -6,12 +6,30 @@ import scipy as sp
 import numpy as np
 import MomentAnsatz
 
+# number of ansatz,  combinations of multiple ansatz x^alpha (1-x)^beta might be needed for more flexiblity but 1 should be good to start with
+init_NumofAnsatz = 1
+# initial parameters for the non-singlet quark distributions
+Para_NS
+
+# initial parameters for the singlet quark distributions
+Para_S
+
+# initial parameters for the gluon distributions
+Para_G
+
+# 
 class GPDobserv (object) :
+    #Initialization of observables. Each is a function of (x, \xi ,t, Q) 
     def __init__(self, init_x , init_xi, init_t, init_Q) -> None:
         self.x = init_x
         self.xi = init_xi
         self.t = init_t
         self.Q = init_Q
-    
-    def tPDFevo(Fu: MomentAnsatz.GPDMoment, Fd : MomentAnsatz.GPDMoment,Fg : MomentAnsatz.GPDMoment):
-        return Fu, Fd, Fg
+        # number of ansatz,  combinations of multiple ansatz x^alpha (1-x)^beta might be needed for more flexiblity but 1 should be good to start with
+        self.NumofAnsatz = init_NumofAnsatz
+
+    #Non-singlet quark distributions in the moment space 
+    def F_NS(self):
+
+    def tPDFevo(self):
+        return F_NS, F_S, Fg
