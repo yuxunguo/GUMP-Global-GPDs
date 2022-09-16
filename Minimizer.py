be524c9eb014ef79d4ba5b5aefe1a0b410c2ac44
yuxunguo <youuungx#gmail>
2022-09-16 14:12:19 -0400

    Upd

--- Minimizer.py
+++ Minimizer.py
@@ -19,9 +19,6 @@ def cost_GUMP(alphapV_H, alphapS_H, R_H_xi2, R_E, R_E_xi2, alphapV_Ht, alphapS_H
     
     cost_tPDF = np.sum(((0 - tPDF_data["f"])/ tPDF_data["delta f"]) ** 2 )
 
-
-
-
 fit_gump = Minuit(cost_GUMP, alphapV_H = 1, alphapS_H = 1, R_H_xi2 = 1, R_E = 1, R_E_xi2 = 1, alphapV_Ht = 1, alphapS_Ht = 1, R_Ht_xi2 = 1, R_Et = 1, R_Et_xi2 = 1)
 fit_gump.errordef = 1
 fit_gump.migrad()
