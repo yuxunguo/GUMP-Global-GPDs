32226b06a06643cf9d02f167572ca39bf6bd3075
yjh-bill <yangjinghong@ucla.edu>
2022-12-04 21:49:22 -0500

    interface

--- Predictions.py
+++ Predictions.py
@@ -15,17 +15,17 @@ Para_Pol = ParaManager_Pol(para_list_pol)
 Para_All = np.stack([Para_Unp, Para_Pol], axis=0)
 
 
-def PDF(xs, ts, Qs, flvs, spes):
+def PDF(x, t, Q, flv, spe):
     '''
     Return parton distribution function (PDF) at a given point.
 
     Args:
-        xs: float or numpy array. 
-        ts: float or numpy array. 
-        Qs: float or numpy array. 
-        flvs: string or an array of string.
+        x: float or numpy array. 
+        t: float or numpy array. 
+        Q: float or numpy array. 
+        flv: string or an array of string.
             flvs is the flavor. It can be 'u', 'd', 'g', 'NS', or 'S'
-        spes: integer or numpy array of integer
+        spe: integer or numpy array of integer
             spes is the "species."
             0 means H
             1 means E
@@ -37,18 +37,18 @@ def PDF(xs, ts, Qs, flvs, spes):
             Parton distribution function.
     '''
     # [x, t, Q, f, delta_f, spe, flv] = PDF_input
-    _is_scalar = ( np.isscalar(xs) and np.isscalar(ts) and np.isscalar(Qs)\
-        and np.isscalar(flvs) and np.isscalar(spes) )
+    _is_scalar = ( np.isscalar(x) and np.isscalar(t) and np.isscalar(Q)\
+        and np.isscalar(flv) and np.isscalar(spe) )
 
-    xs = np.array(xs)
-    ts = np.array(ts)
-    Qs = np.array(Qs)
-    flvs = np.array(flvs)
-    spes = np.array(spes)
+    x = np.array(x)
+    t = np.array(t)
+    Q = np.array(Q)
+    flv = np.array(flv)
+    spe = np.array(spe)
  
     xi = 0
 
-    ps = np.where(spes<=1, 1, -1)
+    p = np.where(spes<=1, 1, -1)
 
     '''
     if(spe == 0 or spe == 1):
@@ -60,7 +60,7 @@ def PDF(xs, ts, Qs, flvs, spes):
     # Para: (4, 2, 5, 1, 4)
 
     Para_spe = Para_All[spes] # fancy indexing. Output (N, 3, 5, 1, 5)
-    _PDF_theo = GPDobserv(xs, xi, ts, Qs, ps)
+    _PDF_theo = GPDobserv(x, xi, t, Q, p)
     _pdf = _PDF_theo.tPDF(flvs, Para_spe)  # array length N
 
     if _is_scalar:
@@ -71,17 +71,17 @@ def PDF(xs, ts, Qs, flvs, spes):
 tPDF = PDF
 
 
-def GFF_theo(js, ts, Qs, flvs, spes):
+def GFF_theo(j, t, Q, flv, spe):
     '''
     Return Generalized Form Factors
 
     Args:
-        js: int or numpy int array
-        ts: float or numpy array
-        Qs:float or numpy array
-        flvs: string or an array of string.
+        j: int or numpy int array
+        t: float or numpy array
+        Q:float or numpy array
+        flv: string or an array of string.
             flvs is the flavor. It can be 'u', 'd', 'g', 'NS', or 'S'
-        spes: integer or numpy array of integer
+        spe: integer or numpy array of integer
             spes is the "species."
             0 means H
             1 means E
@@ -91,14 +91,14 @@ def GFF_theo(js, ts, Qs, flvs, spes):
     Returns:
         _gff: generalized form factors. Float or numpy array
     '''
-    _is_scalar = ( np.isscalar(js) and np.isscalar(ts) and np.isscalar(Qs)\
-        and np.isscalar(flvs) and np.isscalar(spes) )
+    _is_scalar = ( np.isscalar(j) and np.isscalar(t) and np.isscalar(Q)\
+        and np.isscalar(flv) and np.isscalar(spe) )
 
-    js = np.array(js)
-    ts = np.array(ts)
-    Qs = np.array(Qs)
-    flvs = np.array(flvs)
-    spes = np.array(spes)
+    j = np.array(j)
+    t = np.array(t)
+    Q = np.array(Q)
+    flv = np.array(flv)
+    spe = np.array(spe)
     x = 0
     xi = 0   
     '''
@@ -108,14 +108,89 @@ def GFF_theo(js, ts, Qs, flvs, spes):
     if(spe == 2 or spe == 3):
         p = -1
     '''
-    ps = np.where(spes<=1, 1, -1)
+    p = np.where(spes<=1, 1, -1)
    
 
     Para_spe = Para_All[spes] # fancy indexing. Output (N, 3, 5, 1, 5)
-    _GFF_theo = GPDobserv(x, xi, ts, Qs, ps)
-    _gff = _GFF_theo.GFFj0(js, flvs, Para_spe) # (N)
+    _GFF_theo = GPDobserv(x, xi, t, Q, p)
+    _gff = _GFF_theo.GFFj0(j, flv, Para_spe) # (N)
 
     if _is_scalar:
         _gff = _gff.item()
     
     return _gff
+
+
+def CFF(xB, t, Q):
+    '''
+    CFF
+
+    Args
+        xB: float or numpy array
+        t: float or numpy array
+        Q: float or numpy array
+
+    Returns
+        [ HCFF, ECFF, HtCFF, EtCFF ], each of which can be a scalar or numpy array
+    '''
+    xB  = np.array(xB)
+    t   = np.array(t)
+    Q   = np.array(Q)
+    if np.ndim(xB*t*Q)==0:
+        return CFF_theo(xB, t, Q, Para_Unp, Para_Pol)
+    return CFF_theo(xB, t, Q, np.expand_dims(Para_Unp, axis=0), np.expand_dims(Para_Pol, axis=0))
+
+
+def DVCSxsec(y, xB, t, Q, phi, pol):
+    '''
+    DVCS cross section
+
+    Args
+        y: float or numpy array
+        xB: float or numpy array
+        t: float or numpy array
+        Q: float or numpy array
+        phi: float or numpy array
+        pol: string or a numpy array of str
+            pol can be 'UU', 'LU', 'UL', 'LL',
+                'UTin', 'LTin', 'UTout', or 'LTout'
+
+    Returns
+        The DVCS cross section. Float or numpy array
+    '''
+    y   = np.array(y)
+    xB  = np.array(xB)
+    t   = np.array(t)
+    Q   = np.array(Q)
+    phi = np.array(phi)
+    pol = np.array(pol)
+
+    [HCFF, ECFF, HtCFF, EtCFF] = CFF(xB, t, Q)
+    return dsigma_TOT(y, xB, t, Q, phi, pol, HCFF, ECFF, HtCFF, EtCFF)
+
+
+def DVCSxsec_HERA(y, xB, t, Q, pol):
+    '''
+    The total DVCS cross-section integrated over phi
+
+    Args
+        y: float or numpy array
+        xB: float or numpy array
+        t: float or numpy array
+        Q: float or numpy array
+        phi: float or numpy array
+        pol: string or a numpy array of str
+            pol can be 'UU', 'LU', 'UL', 'LL',
+                'UTin', 'LTin', 'UTout', or 'LTout'
+
+    Returns
+        The total DVCS cross-section integrated over phi. Float or numpy array
+    '''
+    y   = np.array(y)
+    xB  = np.array(xB)
+    t   = np.array(t)
+    Q   = np.array(Q)
+    pol = np.array(pol)
+
+    [HCFF, ECFF, HtCFF, EtCFF] = CFF(xB, t, Q)
+    return dsigma_DVCS_HERA(y, xB, t, Q, pol, HCFF, ECFF, HtCFF, EtCFF)
