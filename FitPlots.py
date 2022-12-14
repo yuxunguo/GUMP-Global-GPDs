3b4c51125179e6de301a84df430f8b938add7044
yuxunguo <youuungx#gmail>
2022-12-14 18:50:40 -0500

    Upd

--- FitPlots.py
+++ FitPlots.py
@@ -9,6 +9,14 @@ import csv
 from  Plotting import plot_compare
 from Minimizer import Q_threshold, xB_Cut, DVCSxsec_theo, CFF_theo, DVCSxsec_HERA_theo
 
+df_Para = pd.read_csv('GUMP_Params/params.csv', index_col=0)
+para_list_unp = df_Para['value'][:38].to_numpy()
+para_list_pol = df_Para['value'][38:].to_numpy()
+
+Para_Unp = ParaManager_Unp(para_list_unp)
+Para_Pol = ParaManager_Pol(para_list_pol)
+Para_All = np.concatenate([Para_Unp, Para_Pol], axis=0)
+
 PDF_data = pd.read_csv('GUMPDATA/PDFdata.csv',       header = None, names = ['x', 't', 'Q', 'f', 'delta f', 'spe', 'flv'],        dtype = {'x': float, 't': float, 'Q': float, 'f': float, 'delta f': float,'spe': int, 'flv': str})
 PDF_data_H  = PDF_data[PDF_data['spe'] == 0]
 PDF_data_E  = PDF_data[PDF_data['spe'] == 1]
@@ -45,20 +53,14 @@ def DVCSxsec_cost_xBtQ_plt(DVCSxsec_data_xBtQ: np.array, Para_Unp, Para_Pol):
 
 if __name__ == '__main__':
     pool = Pool()
-    Paralst_Unp     = [4.922534295590349, 0.21634143179981935, 3.228610042709383, 2.3482459294135394, 0.16344275580252743, 1.1357400684186936, 6.895402708196721, 0.15, 3.3586572239218433, 0.1842426100086847, 4.417573382186983, 3.479117270371762, 0.24917750176639988, 1.0519216118153967, 6.551152830038727, 2.8642811429092463, 1.0523058466461475, 7.412779748354998, 0.15, 0.17039078923369722, 0.9117778159622452, 1.0586925188952074, 0.43575743065323375, -0.209955152625657, -0.5256575429614562, -1.3600129447064717, -19.770730039892825, 1.0, 10.267159566549998, -3.2467026195137683, 1.0, 0.37811459327629876, 5.693312161571689, 0.0, -2.1297383785670863, 0.6575758917146275, 0.0, 4.600577817679961]
-    Paralst_Pol     = [4.51444669636376, -0.24542434834709703, 3.0318516798470454, 2.6220577389407684, 0.0765192697023151, 0.5149009611155857, 4.374109637271134, 0.15, -0.7101235808764668, 0.21248116095909975, 3.232764463064717, 4.4410688663255105, -0.055617289814115935, 0.6147308342956928, 2.077852173660539, 0.24325052551974666, 0.6308206072396163, 2.7184278210766615, 0.15, 8.786112107139164, 0.7999996653919439, 7.2970857442157655, 1.9946659519505927, -3.496916052403074, -22.311808008911193, 0.12347264320433687, 42.0289770774691, 1.0, -3.895036041601817, -2.1779671813414163, 1.0, -0.12885271382282817, -12.338040498859877, 0.0, 0.7759791414166142, 36.25529526389492, 0.0, 0.04486971215711455]
-    
-    Para_Unp_All    = ParaManager_Unp(Paralst_Unp)
-    Para_Pol_All    = ParaManager_Pol(Paralst_Pol)
 
-    print(CFF_theo(0.36, -0.345, 2, Para_Unp_All, Para_Pol_All))
+    print(CFF_theo(0.36, -0.345, 2, Para_Unp, Para_Pol))
 
-    print(CFF_theo(0.48, -0.702, 2, Para_Unp_All, Para_Pol_All))
+    print(CFF_theo(0.48, -0.702, 2, Para_Unp, Para_Pol))
 
-    print(CFF_theo(0.60, -1.05, 2, Para_Unp_All, Para_Pol_All))
+    print(CFF_theo(0.60, -1.05, 2, Para_Unp, Para_Pol))
 
-    """
-    DVCS_HERA_pred = np.transpose([DVCSxsec_HERA_theo(DVCS_HERA_data, Para_Unp=Para_Unp_All, Para_Pol=Para_Pol_All)])
+    DVCS_HERA_pred = np.transpose([DVCSxsec_HERA_theo(DVCS_HERA_data, Para_Unp=Para_Unp, Para_Pol=Para_Pol)])
     with open("GUMP_Results/HERAxsec.csv","w",newline='') as my_csv:
         csvWriter = csv.writer(my_csv,delimiter=',')
         csvWriter.writerows(DVCS_HERA_pred)
@@ -67,13 +69,9 @@ if __name__ == '__main__':
     #print(xsecgp0)
     xsecgp0UU = xsecgp0[xsecgp0['pol'] == 'UU']
     xsecgp0LU = xsecgp0[xsecgp0['pol'] == 'LU']
-    xsecgp0UU_pred, xsecgp0HCFF, xsecgp0ECFF, xsecgp0HtCFF, xsecgp0EtCFF =DVCSxsec_cost_xBtQ_plt(xsecgp0UU, Para_Unp_All,Para_Pol_All )
-    xsecgp0LU_pred, xsecgp0HCFF, xsecgp0ECFF, xsecgp0HtCFF, xsecgp0EtCFF =DVCSxsec_cost_xBtQ_plt(xsecgp0LU, Para_Unp_All,Para_Pol_All )
+    xsecgp0UU_pred, xsecgp0HCFF, xsecgp0ECFF, xsecgp0HtCFF, xsecgp0EtCFF =DVCSxsec_cost_xBtQ_plt(xsecgp0UU, Para_Unp,Para_Pol)
+    xsecgp0LU_pred, xsecgp0HCFF, xsecgp0ECFF, xsecgp0HtCFF, xsecgp0EtCFF =DVCSxsec_cost_xBtQ_plt(xsecgp0LU, Para_Unp,Para_Pol)
 
-    print(xsecgp0HCFF, xsecgp0ECFF, xsecgp0HtCFF, xsecgp0EtCFF)
-    #plot_compare(xsecgp0UU['phi'], xsecgp0UU['f'], xsecgp0UU['delta f'], xsecgp0UU_pred)
-    #plot_compare(xsecgp0LU['phi'], xsecgp0LU['f'], xsecgp0LU['delta f'], xsecgp0LU_pred)
-    
     xsecUUgp0 = np.transpose(np.array([xsecgp0UU['phi'], xsecgp0UU['f'], xsecgp0UU['delta f'], xsecgp0UU_pred]))
     xsecLUgp0 = np.transpose(np.array([xsecgp0LU['phi'], xsecgp0LU['f'], xsecgp0LU['delta f'], xsecgp0LU_pred]))
 
@@ -84,7 +82,7 @@ if __name__ == '__main__':
     with open("GUMP_Results/LUxsec.csv","w",newline='') as my_csv:
         csvWriter = csv.writer(my_csv,delimiter=',')
         csvWriter.writerows(xsecLUgp0)
-    """
+
     """
     xsecgp1 = DVCSxsec_group_data[3]
     print(xsecgp1)
