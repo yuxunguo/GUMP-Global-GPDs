f70e2b4733f2988b17fde697f10258cf9da524b6
Yuxun Guo <youuungx@gmail.com>
2024-12-03 16:00:09 -0800

    upd gitignore and change predictions for consistency

--- Predictions.py
+++ Predictions.py
@@ -219,7 +219,9 @@ if __name__ == '__main__':
     #
 
     # Comparing PDF with the global extraction of PDF
-    '''
+
+    os.makedirs(os.path.join(dir_path,"GUMP_Results"), exist_ok=True)
+    
     x = np.exp(np.linspace(np.log(0.0001), np.log(0.05), 100, dtype = float))
     
     pdflst = np.array([PDF_theo_s(x_i,0.,2.,1,'g',Para_spe, 2) for x_i in x ])
@@ -239,6 +241,7 @@ if __name__ == '__main__':
         csvWriter = csv.writer(my_csv,delimiter=',')
         csvWriter.writerows(np.transpose([x,gpdlst]))
     print(time.time()-ts)
+
     '''
     # Ploting the R ratio
 
@@ -257,7 +260,7 @@ if __name__ == '__main__':
     with open(os.path.join(dir_path,"GUMP_Results/Rrat2D.csv"),"w",newline='') as my_csv:
         csvWriter = csv.writer(my_csv,delimiter=',')
         csvWriter.writerows(np.transpose([xmeshflat,qmeshflat,rrat2dlst]))
-
+    '''
     # Comparing the cross-sections
     '''
     DVjpsiPH1_xBtQ_theo = np.array(list(pool.map(partial(DVjpsiPxsec_theo_xBtQ, Para_Unp = Para_Unp, xsec_norm = jpsinorm, p_order = 2, muset = 1, flv = 'All'), DVJpsiPH1xsec_group_data))).flatten()
