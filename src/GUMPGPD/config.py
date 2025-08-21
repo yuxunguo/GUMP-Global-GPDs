f903ad824467f1d679d677d8a54048009b2fc315
Yuxun Guo <youuungx@gmail.com>
2025-08-21 08:58:19 -0700

    Remove duplicate data folder

--- src/GUMPGPD/config.py
+++
@@ -1,6 +0,0 @@
-"""
-Not in use currently. Can be used to set config parameters.
-"""
-# Turn it on (set to true) so the cost function will save the results to files
-# Only used after the fit is finished!
-Export_Mode = False
